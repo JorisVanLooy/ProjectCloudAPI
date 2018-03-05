2eb64f2f1f3d0cd7b9dae5bf084a31ab28d20ff7
Joris Van Looy <s096257@ap.be>
2018-03-05 11:11:11 +0100

    brewery service aangemaakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BreweryService} from './services/brewery.service';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    BreweryService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
