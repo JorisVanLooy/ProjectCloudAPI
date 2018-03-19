afc0a10f2a2501c806398ef9bb5a256ee1a3d121
Joris Van Looy <s096257@ap.be>
2018-03-19 10:11:19 +0100

    remove brewery service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BreweryService} from './services/brewery.service';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 
@@ -13,7 +12,7 @@ import {HttpClientModule} from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
-    BreweryService
+    
   ],
   bootstrap: [AppComponent]
 })
